d28de64abd1bd9009cc4b375b2605a5c706faea3
Rishav-R03 <rishav042023@gmail.com>
2025-06-11 18:40:20 +0530

    modified

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker 
-
+import os 
 dburl = URL.create(
     drivername="postgresql",
     username="postgres",
-    password="root123",
+    password=os.getenv("POSTGRESQL_PASSWORD"),
     database="smartresearchagent",
-    port=5432
+    port=5432,
+    host="localhost"
 )
 
 engine = create_engine(dburl)
